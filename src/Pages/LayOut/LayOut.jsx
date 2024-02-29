f61b8db456c4a3fb3d5c8d482478e5c8aa4c9b9b
ahmed samir <ahmedSamir@gmail.com>
2024-02-29 21:46:56 +0200

    init

---
+++ src/Pages/LayOut/LayOut.jsx
@@ -0,0 +1,25 @@
+import React, { useContext, useEffect } from "react";
+import "./LayOut.module.css";
+import Navbar from "../../Components/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "../../Components/Footer/Footer";
+import { UserContext } from "../../Context/UserContext";
+import { Toaster } from "react-hot-toast";
+
+export default function LayOut() {
+  const { setUserToken } = useContext(UserContext);
+  useEffect(() => {
+    if (localStorage.getItem("userToken")) {
+      setUserToken(localStorage.getItem("userToken"));
+    }
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <Outlet></Outlet>
+      <Toaster />
+      <Footer />
+    </>
+  );
+}
